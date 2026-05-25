b7f4c1f538def63472b622078d3e02a3bf121ff8
neppiness <neppiness@gmail.com>
2026-05-25 17:03:48 +0900

    [#2] 주스탯에 따라 증가되는 스탯을 묶도록 배치 수정

--- src/main/kotlin/com/ether/stat/MainStats.kt
+++ src/main/kotlin/com/ether/stat/MainStats.kt
@@ -1,9 +1,9 @@
 package com.ether.stat
 
 data class MainStats(
-    val strength: Int, // 힘
-    val skill: Int, // 솜씨
+    val strength: Int,     // 힘
+    val skill: Int,        // 솜씨
     val intelligence: Int, // 지력
-    val will: Int, // 의지
-    val luck: Int, // 행운
+    val will: Int,         // 의지
+    val luck: Int,         // 행운
 )
--- src/main/kotlin/com/ether/stat/SubStats.kt
+++ src/main/kotlin/com/ether/stat/SubStats.kt
@@ -1,23 +1,87 @@
 package com.ether.stat
 
 data class SubStats(
-    val attackPower: Int,            // 공격력
-    val defense: Int,                // 방어력
+    val attack: Int = 0,                 // 공격력
+    val defense: Int = 0,                // 방어력
 
-    val breaking: Int,               // 브레이크
-    val heavyStrike: Int,            // 강타 강화
-    val combo: Int,                  // 콤보 강화
-    val skillPower: Int,             // 스킬 위력
-    val aoeEnhancement: Int,         // 광역 강화
-    val healingPower: Int,           // 회복력
-    val dodge: Int,                  // 급소 회피
-    val additionalHit: Int,          // 추가타
+    val breaking: Int = 0,               // 브레이크
+    val damageReduction: Int = 0,        // 피해 감소
+    val heavyStrike: Int = 0,            // 강타 강화
 
-    val damageReduction: Int,        // 피해 감소
-    val attackBoost: Int,            // 빠른 공격
-    val chainStrike: Int,            // 연타 강화
-    val skillSpeed: Int,             // 빠른 스킬
-    val healthBonus: Int,            // 추가 체력
-    val ultimateSkill: Int,          // 궁극기
-    val criticalHit: Int             // 치명타
-)
+    val attackSpeed: Int = 0,            // 빠른 공격
+    val comboStrike: Int = 0,            // 콤보 강화
+    val chainStrike: Int = 0,            // 연타 강화
+
+    val skillPower: Int = 0,             // 스킬 위력
+    val skillSpeed: Int = 0,             // 빠른 스킬
+    val aoeStrike: Int = 0,              // 광역 강화
+
+    val additionalHealth: Int = 0,       // 추가 체력
+    val heal: Int = 0,                   // 회복력
+    val ultimate: Int = 0,               // 궁극기
+
+    val dodge: Int = 0,                  // 급소 회피
+    val critical: Int = 0,               // 치명타
+    val additionalHit: Int = 0,          // 추가타
+) {
+
+    fun reflection(mainStats: MainStats): SubStats {
+        return SubStats(
+            breaking = mainStats.strength / 4,
+            damageReduction = mainStats.strength / 4,
+            heavyStrike = mainStats.strength / 4,
+
+            attackSpeed = mainStats.skill / 4,
+            comboStrike = mainStats.skill / 4,
+            chainStrike = mainStats.skill / 4,
+
+            skillPower = mainStats.intelligence / 4,
+            skillSpeed = mainStats.intelligence / 4,
+            aoeStrike = mainStats.intelligence / 4,
+
+            additionalHealth = mainStats.will / 4,
+            heal = mainStats.will / 4,
+            ultimate = mainStats.will / 4,
+
+            dodge = mainStats.luck / 4,
+            critical = mainStats.luck / 4,
+            additionalHit = mainStats.luck / 4,
+        )
+    }
+
+    operator fun plus(rightTerm: SubStats): SubStats {
+        return SubStats(
+            attack = attack + rightTerm.attack,
+            defense = defense + rightTerm.defense,
+
+            breaking = breaking + rightTerm.breaking,
+            damageReduction = damageReduction + rightTerm.damageReduction,
+            heavyStrike = heavyStrike + rightTerm.heavyStrike,
+
+            attackSpeed = attackSpeed + rightTerm.attackSpeed,
+            comboStrike = comboStrike + rightTerm.comboStrike,
+            chainStrike = chainStrike + rightTerm.chainStrike,
+
+            skillPower = skillPower + rightTerm.skillPower,
+            skillSpeed = skillSpeed + rightTerm.skillSpeed,
+            aoeStrike = aoeStrike + rightTerm.aoeStrike,
+
+            additionalHealth = additionalHealth + rightTerm.additionalHealth,
+            heal = heal + rightTerm.heal,
+            ultimate = ultimate + rightTerm.ultimate,
+
+            dodge = dodge + rightTerm.dodge,
+            critical = critical + rightTerm.critical,
+            additionalHit = additionalHit + rightTerm.additionalHit,
+        )
+    }
+
+    companion object {
+        fun sum(vararg stats: SubStats): SubStats {
+            return stats.fold(SubStats()) { acc, stat ->
+                acc + stat
+            }
+        }
+    }
+
+}
