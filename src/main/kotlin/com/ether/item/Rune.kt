dfaaa53e5448e46fd64b9bb31f6bff0a97f13d9d
neppiness <neppiness@gmail.com>
2026-05-27 21:23:42 +0900

    [#2] 뇌명, 아득한 빛, 압도적인 힘 룬 초안 구현

--- src/main/kotlin/com/ether/item/Rune.kt
+++ src/main/kotlin/com/ether/item/Rune.kt
@@ -1,15 +1,65 @@
 package com.ether.item
 
 interface Rune {
-    fun getSkillPowerPercent(isAwakening: Boolean): Double = 0.0
-    fun getChainStrikePercent(isAwakening: Boolean): Double = 0.0
-    fun getHeavyStrikePercent(isAwakening: Boolean): Double = 0.0
-    fun getDamagePercent(isAwakening: Boolean): Double = 0.0
-    fun getGivenDamagePercent(isAwakening: Boolean): Double = 0.0
-    fun getAoePercent(isAwakening: Boolean): Double = 0.0
-    fun getComboPercent(isAwakening: Boolean): Double = 0.0
-    fun getCriticalProbabilityPercent(isAwakening: Boolean): Double = 0.0
-    fun getCriticalDamagePercent(isAwakening: Boolean): Double = 0.0
-    fun getVulnerableDamagePercent(isAwakening: Boolean): Double = 0.0
-    fun getAdditionalHitPercent(isAwakening: Boolean): Double = 0.0
+
+    fun getAttackPowerPercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getSkillPowerPercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getChainStrikePercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getHeavyStrikePercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getDamagePercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getGivenDamagePercent( // 적에게 주는 피해 증가량(%)
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getAoePercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getComboPercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getCriticalProbabilityPercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getCriticalDamagePercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getVulnerableDamagePercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
+    fun getAdditionalHitPercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+    ): Double = 0.0
+
 }
