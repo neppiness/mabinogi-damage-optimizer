5e1bd56a0008a7ba8e56dcbc12d868c904de010d
neppiness <neppiness@gmail.com>
2026-05-30 13:19:18 +0900

    [#2] 섬세한 손놀림, 불안정한 힘, 검게 물든 후광 구현

--- src/main/kotlin/com/ether/item/runes/RuneImpls.kt
+++ src/main/kotlin/com/ether/item/runes/RuneImpls.kt
@@ -7,6 +7,7 @@ class Thunder : Rune {
     override fun getHeavyStrikePercent(
         isAwakening: Boolean,
         transcendenceLevel: Int,
+        itemLevel: Int,
     ): Double = when (transcendenceLevel) {
         0 -> 20.0
         1 -> 22.0
@@ -20,6 +21,7 @@ class DistantLight : Rune {
     override fun getCriticalProbabilityPercent(
         isAwakening: Boolean,
         transcendenceLevel: Int,
+        itemLevel: Int,
     ): Double {
         val constant = 5.0
         return constant + if (isAwakening) 20.0 else 0.0
@@ -28,6 +30,7 @@ class DistantLight : Rune {
     override fun getCriticalDamagePercent(
         isAwakening: Boolean,
         transcendenceLevel: Int,
+        itemLevel: Int,
     ): Double {
         val constant = 10.0
         val awakening = if (!isAwakening) 0.0 else when (transcendenceLevel) {
@@ -39,9 +42,10 @@ class DistantLight : Rune {
         return constant + awakening
     }
 
-    override fun getGivenDamagePercent( // 적에게 주는 피해 증가량(%)
+    override fun getGivenDamagePercent(
         isAwakening: Boolean,
         transcendenceLevel: Int,
+        itemLevel: Int,
     ): Double = when (transcendenceLevel) {
         0 -> 5.0
         1 -> 7.0
@@ -55,6 +59,7 @@ class OverwhelmingPower : Rune {
     override fun getAttackPowerPercent(
         isAwakening: Boolean,
         transcendenceLevel: Int,
+        itemLevel: Int,
     ): Double {
         val constant = when (transcendenceLevel) {
             0 -> 6.0
@@ -68,6 +73,7 @@ class OverwhelmingPower : Rune {
     override fun getHeavyStrikePercent(
         isAwakening: Boolean,
         transcendenceLevel: Int,
+        itemLevel: Int,
     ): Double {
         val constant = 6.0
         return if (isAwakening) constant * 2 else constant
@@ -76,8 +82,74 @@ class OverwhelmingPower : Rune {
     override fun getCriticalProbabilityPercent(
         isAwakening: Boolean,
         transcendenceLevel: Int,
+        itemLevel: Int,
     ): Double {
         val constant = 4.0
         return if (isAwakening) constant * 2 else constant
+    }
+}
+
+// 섬세한 손놀림
+class DelicateHandling : Rune {
+    override fun getAttackPowerPercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+        itemLevel: Int,
+    ): Double {
+        val constant = when (transcendenceLevel) {
+            0 -> 6.0
+            1 -> 8.0
+            2 -> 10.0
+            else -> throw IllegalArgumentException("잘못된 초월 레벨입니다.")
+        }
+        return if (isAwakening) constant * 2 else constant
+    }
+
+    override fun getChainStrikePercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+        itemLevel: Int,
+    ): Double {
+        val constant = 6.0
+        return if (isAwakening) constant * 2 else constant
+    }
+
+    override fun getAdditionalHitPercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+        itemLevel: Int,
+    ): Double {
+        val constant = 4.0
+        return if (isAwakening) constant * 2 else constant
+    }
+}
+
+// 불안정한 힘
+class UnstablePower : Rune {
+    override fun getGivenDamagePercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+        itemLevel: Int,
+    ): Double = when (transcendenceLevel) {
+        0 -> 22.0
+        1 -> 24.0
+        2 -> 26.0
+        else -> throw IllegalArgumentException("잘못된 초월 레벨입니다.")
+    }
+}
+
+// 검게 물든 후광
+class DarkenHalo : Rune {
+    override fun getGivenDamagePercent(
+        isAwakening: Boolean,
+        transcendenceLevel: Int,
+        itemLevel: Int,
+    ): Double {
+        val constant = 15.0
+        return when {
+            itemLevel >= 15 -> constant + 12;
+            itemLevel >= 10 -> constant + 6;
+            else -> constant;
+        }
     }
 }
